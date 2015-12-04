518ca95ed6102bc5fdcd88edcaf64cad271f6bb4
MiniWitch3 <med94man@yahoo.com>
2015-12-03 19:16:25 -0800

    just a condition

--- models/tables.py
+++ models/tables.py
@@ -3,7 +3,7 @@ from datetime import datetime
 db.define_table('pets',
                 Field("name", requires = IS_NOT_EMPTY),
                 Field("author", db.auth_user, default = auth.user_id),
-                Field("description", "text"),
+                Field("description", "text", requires = IS_NOT_EMPTY),
                 Field("posted", 'datetime'),
                 Field("pet_image", 'upload', default = 'path/'),
                 Field("house_trained", default = False),
